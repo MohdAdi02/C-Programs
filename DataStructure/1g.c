cdf991101f5e409dd77a76a2df35b1ce3c1e2e59
LAPTOP-AU0SMQI8\mohd adil <md7248699370@gmail.com>
2024-01-29 19:47:00 +0530

    modified

--- DataStructure/1g.c
+++ DataStructure/1g.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 int main (){
-    int a = 5, b = 5;
-    printf("%d",a+b);
+    int a;
+    int b;
+    int result;
+    printf("enter the first value: ");
+    scanf("\n%d",&a);
+    printf("enter the second value: ");
+    scanf("\n%d",&b);
+    result = a+b;
+    printf("the sum is : ",result);
 
     return 0;
 }
