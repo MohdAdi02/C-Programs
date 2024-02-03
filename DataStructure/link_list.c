024534a0a7fc26c61874b2017df0147c7c55b984
LAPTOP-AU0SMQI8\mohd adil <md7248699370@gmail.com>
2024-02-03 08:36:51 +0530

    adding funcitons

--- DataStructure/link_list.c
+++ DataStructure/link_list.c
@@ -4,14 +4,56 @@
 #include<stdlib.h>
 
 struct node {
-int data;
-struct node 
+    int data;
+    struct node *next; 
+
+};
+
+
+void inset(int x,struct node *head){
 
 }
 
+void traverse(struct node *head){
+
+}
+
+void delete_from_end(struct node *head){
+
+}
 
 int main(){
 
+    int choice;
+    struct node *head = NULL;
+    int num;
 
 
+    while(1){
+        printf("Enter a choice\n");
+        printf("Insert element in list\n");
+        printf("Traverse link list");
+        printf("Delete an element from end");
+        printf("Exit");
+        scanf("%d",&choice);
+
+        switch(choice){
+            case 1: printf("Enter the element to add in linklist");
+                    scanf("%d",&num);
+                    insert(num,head);
+                break;
+            case 2: traverse(head);
+                break;
+            case 3: delete_from_end(head);
+                break;
+            case 4:exit(0);
+                break;
+        }
+
+    }
+    
+
+
+
+    return 0;
 }
